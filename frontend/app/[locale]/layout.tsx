4a6fa1976920b18260d6235a351db523d863b903
Johannes Wild <jw@moonich.de>
2025-11-11 13:46:57 +0100

    🐛 Fix layout structure: remove root layout, locale layout provides full HTML

--- frontend/app/[locale]/layout.tsx
+++ frontend/app/[locale]/layout.tsx
@@ -86,16 +86,18 @@ export default async function LocaleLayout({
   const messages = await getMessages();
 
   return (
-    <NextIntlClientProvider locale={locale} messages={messages}>
+    <html lang={locale} suppressHydrationWarning>
       <body className={`${GeistSans.variable} ${GeistMono.variable} antialiased font-sans min-h-screen`}>
-        {/* Border lines - bottom and sides only */}
-        <div className="fixed bottom-0 left-0 right-0 h-[2px] bg-dark-border z-50" />
-        <div className="fixed top-0 left-0 bottom-0 w-[2px] bg-dark-border z-50" />
-        <div className="fixed top-0 right-0 bottom-0 w-[2px] bg-dark-border z-50" />
-        
-        {children}
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {/* Border lines - bottom and sides only */}
+          <div className="fixed bottom-0 left-0 right-0 h-[2px] bg-dark-border z-50" />
+          <div className="fixed top-0 left-0 bottom-0 w-[2px] bg-dark-border z-50" />
+          <div className="fixed top-0 right-0 bottom-0 w-[2px] bg-dark-border z-50" />
+          
+          {children}
+        </NextIntlClientProvider>
       </body>
-    </NextIntlClientProvider>
+    </html>
   );
 }
 
