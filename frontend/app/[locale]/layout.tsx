5d464e0ac11dea0921a5c75aca62dfeba3a30215
Johannes Wild <jw@moonich.de>
2025-11-12 08:05:07 +0100

    ✅ Add NextIntlClientProvider back with getMessages()

--- frontend/app/[locale]/layout.tsx
+++ frontend/app/[locale]/layout.tsx
@@ -1,3 +1,5 @@
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { locales } from '@/i18n';
 import "../globals.css";
@@ -11,7 +13,7 @@ export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
 }
 
-export default function LocaleLayout({ children, params }: Props) {
+export default async function LocaleLayout({ children, params }: Props) {
   const { locale } = params;
   
   // Validate locale
@@ -19,10 +21,15 @@ export default function LocaleLayout({ children, params }: Props) {
     notFound();
   }
   
+  // Providing all messages to the client side is the easiest way to get started
+  const messages = await getMessages();
+  
   return (
     <html lang={locale}>
       <body style={{ margin: 0, padding: 0 }}>
-        {children}
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
       </body>
     </html>
   );
